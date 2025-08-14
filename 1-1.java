6525d4c8610c7f7b89df01bc77ef700688570273
Aditya Jain <jainaditya412@gmail.com>
2025-08-14 09:12:04 +0530

    Create 1-1.java
    final

---
+++ 1-1.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class FirstLecture {
+    public static void main(String[] args)
+    {
+        // System.out.print("Aditya Jain Sir welcomes you all to Java lectures");
+        // System.out.print("Aditya Jain Sir");
+        // system.out.print("\n");
+        
+        System.out.println("Aditya Jain Sir welcomes you all to Java lectures");
+        System.out.println("Aditya Jain Sir");
+    }
+}
