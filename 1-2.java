88b580171fb7a77f32c145f03bac4797057f09c9
Aditya Jain <jainaditya412@gmail.com>
2025-08-18 09:12:33 +0530

    Create 1-2.java
    final

---
+++ 1-2.java
@@ -0,0 +1,22 @@
+class Main
+{
+    public String greetUser(String name)
+    {
+        System.out.println("Hello", + name);
+    }
+    
+     public String greet_user(String name)
+    {
+        System.out.println("Hello", + name);
+    }
+    
+    public static void main(String[] args)
+    {
+        int price = 19;
+        
+        String firstName = "AJ";
+        String first_name = "AJ";
+        
+        System.out.printf("Price is: %d", price);
+    }
+}
